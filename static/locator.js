024bd3e021453c7040530298ef40f8f816194933
waheng chang <wahengchang@gmail.com>
2018-09-05 23:35:11 +0800

    9b6d9c8

---
+++ static/locator.js
@@ -0,0 +1,9 @@
+
+    window.ENV =  "undefined"
+    window.FIREBASE_API_KEY = "AIzaSyB-Oc9OpqbZROu7y1Rr1mk_6_CV0STA9jQ",
+    window.FIREBASE_AUTH_DOMAIN = "voice-messager.firebaseapp.com",
+    window.FIREBASE_DB_URL = "https://voice-messager.firebaseio.com",
+    window.FIREBASE_PROJECT_ID = "voice-messager",
+    window.FIREBASE_MESSAGEING_ID = "305201925866",
+    window.FIREBASE_STORAGE_BUCKET = "voice-messager.appspot.com"
+    
